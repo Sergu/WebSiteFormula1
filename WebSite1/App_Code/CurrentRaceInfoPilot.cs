cb9a469330039a3fb8d59f892e622ca94aa712a7
Sergu <sergushov.1995@mail.ru>
2015-04-14 15:48:06 +0300

    Some Info Contents without rss

---
+++ WebSite1/App_Code/CurrentRaceInfoPilot.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Сводное описание для CurrentRaceInfoPilot
+/// </summary>
+public class CurrentRaceInfoPilot
+{
+    public string Position { get; set; }
+    public string Driver { get; set; }
+    public string Team { get; set; }
+    public string Grid { get; set; }
+    public string Laps { get; set; }
+    public string Status { get; set; }
+    public string Time { get; set; }
+    public string FastestLap { get; set; }
+    public string ImageCar { get; set; }
+}
---
+++ WebSite1/App_Code/DriverInfoPilot.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Сводное описание для DriverInfoPilot
+/// </summary>
+public class DriverInfoPilot
+{
+	public string PermanentNumber{get;set;}
+    public string GivenName { get; set; }
+    public string FamilyName { get; set; }
+    public string DateOfBirth { get; set; }
+    public string Nationality { get; set; }
+}
